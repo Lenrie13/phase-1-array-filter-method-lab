01bbf04ef6d0913dcd25c839e8661f19840121ff
Lenrie13 <lenrie.ogutu13@gmail.com>
2024-07-14 16:33:55 +0300

    Update array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+// Matching list of drivers
+const findMatching = (drivers, name) => {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+// Finding drivers whose names start with the given letters
+const fuzzyMatch = (drivers, letters) => {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+// Matching driver objects by name property
+const matchName = (drivers, name) => {
+    return drivers.filter(driver => driver.name === name);
+}
